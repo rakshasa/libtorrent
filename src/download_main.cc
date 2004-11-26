596195a9a1e362bf57a2b70199037b725ac1e23a
rakshasa <sundell.software@gmail.com>
2004-11-26 18:51:48 +0000

    Splitting download_main.cc up to so it might compile on my 64mb ram boxen.
    git-svn-id: svn://rakshasa.no/libtorrent/trunk/libtorrent@173 e378c898-3ddf-0310-93e7-cc216c733640

--- src/download_main.cc
+++ src/download_main.cc
@@ -1,7 +1,6 @@
 #include "config.h"
 
 #include <sigc++/signal.h>
-#include <sigc++/hide.h>
 
 #include "torrent/exceptions.h"
 #include "net/listen.h"
@@ -17,7 +16,6 @@
 
 #include <sstream>
 #include <limits>
-#include <unistd.h>
 #include <algo/algo.h>
 
 using namespace algo;
@@ -290,58 +288,6 @@ DownloadMain::receive_download_done() {
   m_tracker->send_state(TRACKER_COMPLETED);
 
   caughtExceptions.push_back("Sendt completed to tracker");
-}
-
-void
-DownloadMain::setup_delegator() {
-  m_net.get_delegator().get_select().set_bitfield(&m_state.content().get_bitfield());
-  m_net.get_delegator().get_select().set_seen(&m_state.bfCounter());
-
-  m_net.get_delegator().get_select().get_priority().add(Priority::NORMAL, 0, m_state.content().get_storage().get_chunkcount());
-
-  m_net.get_delegator().signal_chunk_done().connect(sigc::mem_fun(m_state, &DownloadState::chunk_done));
-  m_net.get_delegator().slot_chunk_size(sigc::mem_fun(m_state.content(), &Content::get_chunksize));
-
-  m_slotChunkPassed = sigc::mem_fun(m_net.get_delegator(), &Delegator::done);
-  m_slotChunkFailed = sigc::mem_fun(m_net.get_delegator(), &Delegator::redo);
-}
-
-void
-DownloadMain::setup_net() {
-  m_net.set_settings(&m_state.settings());
-
-  m_net.slot_chunks_completed(sigc::mem_fun(m_state.content(), &Content::get_chunks_completed));
-  m_net.slot_chunks_count(sigc::mem_fun(m_state.content().get_storage(), &Storage::get_chunkcount));
-
-  m_state.signal_chunk_passed().connect(sigc::hide(sigc::mem_fun(m_net, &DownloadNet::update_endgame)));
-}
-
-void
-DownloadMain::setup_tracker(const bencode& b) {
-  m_tracker = new TrackerControl(m_state.me(), m_state.hash(), generateKey());
-
-  m_tracker->add_url(b["announce"].asString());
-
-  m_tracker->signal_peers().connect(sigc::mem_fun(*this, &DownloadMain::add_peers));
-  m_tracker->signal_peers().connect(sigc::hide(m_signalTrackerSucceded.make_slot()));
-
-  m_tracker->signal_failed().connect(sigc::mem_fun(caughtExceptions, (void (std::list<std::string>::*)(const std::string&))&std::list<std::string>::push_back));
-
-  m_tracker->slot_stat_down(sigc::mem_fun(m_net.get_rate_down(), &Rate::total));
-  m_tracker->slot_stat_up(sigc::mem_fun(m_net.get_rate_up(), &Rate::total));
-  m_tracker->slot_stat_left(sigc::mem_fun(m_state, &DownloadState::bytes_left));
-}
-
-void
-DownloadMain::setup_start() {
-  m_state.signal_chunk_passed().connect(m_slotChunkPassed);
-  m_state.signal_chunk_failed().connect(m_slotChunkFailed);
-}
-
-void
-DownloadMain::setup_stop() {
-  m_slotChunkPassed.disconnect();
-  m_slotChunkFailed.disconnect();
 }
 
 }
