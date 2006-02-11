8d3fc55bc4d75e2d4a9272471d579549e377c058
rakshasa <sundell.software@gmail.com>
2006-02-11 15:34:53 +0000

    * Expand ~ in paths, all uses should be covered.
    * Moved option file parsing to VariableMap and added "import" and
    "try_import" options for loading option files.
    
    
    git-svn-id: svn://rakshasa.no/libtorrent/trunk/libtorrent@636 e378c898-3ddf-0310-93e7-cc216c733640

---
+++ rak/path.h
@@ -0,0 +1,63 @@
+// rak - Rakshasa's toolbox
+// Copyright (C) 2005-2006, Jari Sundell
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// In addition, as a special exception, the copyright holders give
+// permission to link the code of portions of this program with the
+// OpenSSL library under certain conditions as described in each
+// individual source file, and distribute linked combinations
+// including the two.
+//
+// You must obey the GNU General Public License in all respects for
+// all of the code used other than OpenSSL.  If you modify file(s)
+// with this exception, you may extend this exception to your version
+// of the file(s), but you are not obligated to do so.  If you do not
+// wish to do so, delete this exception statement from your version.
+// If you delete this exception statement from all source files in the
+// program, then also delete it here.
+//
+// Contact:  Jari Sundell <jaris@ifi.uio.no>
+//
+//           Skomakerveien 33
+//           3185 Skoppum, NORWAY
+
+// Various functions for manipulating file paths. Also consider making
+// a directory iterator.
+
+#ifndef RAK_PATH_H
+#define RAK_PATH_H
+
+#include <cstdlib>
+#include <string>
+
+namespace rak {
+
+inline std::string
+path_expand(const std::string& path) {
+  if (path.empty() || path[0] != '~')
+    return path;
+
+  char* home = std::getenv("HOME");
+
+  if (home == NULL)
+    return path;
+  
+  return home + path.substr(1);
+}
+
+}
+
+#endif
