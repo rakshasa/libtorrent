79e7e26edcc725e5d7bc34ae1395d02398dd1c80
Jari Sundell <sundell.software@gmail.com>
2011-12-09 01:41:01 +0900

    Fixed test_requesting_timeout unittest.

--- test/torrent/tracker_controller_test.cc
+++ test/torrent/tracker_controller_test.cc
@@ -546,31 +546,24 @@ tracker_controller_test::test_requesting_timeout() {
   tracker_controller.start_requesting();
   TEST_GOTO_NEXT_TIMEOUT(0);
 
-  // std::cout << std::endl << tracker_list.has_active() << ' ' << tracker_controller.seconds_to_next_timeout() << std::endl;
+  TEST_MULTI3_IS_BUSY("11111", "11111");
 
-  // TEST_MULTI3_IS_BUSY("10000", "10000");
-  // TEST_MULTI3_IS_BUSY("01111", "01111");
+  CPPUNIT_ASSERT(tracker_0_0->trigger_failure());
+  CPPUNIT_ASSERT(tracker_controller.seconds_to_next_timeout() == 5);
+  CPPUNIT_ASSERT(tracker_0_1->trigger_failure());
+  CPPUNIT_ASSERT(tracker_1_0->trigger_failure());
+  CPPUNIT_ASSERT(tracker_2_0->trigger_failure());
+  CPPUNIT_ASSERT(tracker_3_0->trigger_failure());
 
-  // CPPUNIT_ASSERT(tracker_0_1->trigger_success());
-  // CPPUNIT_ASSERT(tracker_1_0->trigger_success());
-  // CPPUNIT_ASSERT(tracker_2_0->trigger_success());
-  // CPPUNIT_ASSERT(tracker_3_0->trigger_success());
+  TEST_GOTO_NEXT_TIMEOUT(5);
 
-  // TODO: Change this so that requesting state results in tracker
-  // requests from many peers. Also, add a limit so we don't keep
-  // requesting from spent trackers.
+  TEST_MULTI3_IS_BUSY("11111", "11111");
 
-  // Next timeout should be soon...
-  // TEST_GOTO_NEXT_TIMEOUT(30);
+  CPPUNIT_ASSERT(!tracker_controller.task_timeout()->is_queued());
+  CPPUNIT_ASSERT(tracker_0_1->trigger_success());
+  CPPUNIT_ASSERT(tracker_controller.seconds_to_next_timeout() == 30);
 
-  // TEST_MULTI3_IS_BUSY("10000", "10000");
-
-  // CPPUNIT_ASSERT(tracker_0_0->trigger_success());
-
-  // tracker_controller.stop_requesting();
-
-  // CPPUNIT_ASSERT(tracker_controller.seconds_to_next_timeout() == tracker_0_0->normal_interval());
-  // TEST_MULTIPLE_END(3, 0);
+  TEST_MULTIPLE_END(1, 5);
 }
 
 void
