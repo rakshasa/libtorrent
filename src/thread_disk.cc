b077409ce6a9297cd611712c63ba05b16d1de5b9
Jari Sundell <sundell.software@gmail.com>
2011-12-23 12:36:24 +0900

    Added missing files.

---
+++ src/thread_disk.cc
@@ -0,0 +1,68 @@
+// libTorrent - BitTorrent library
+// Copyright (C) 2005-2011, Jari Sundell
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// In addition, as a special exception, the copyright holders give
+// permission to link the code of portions of this program with the
+// OpenSSL library under certain conditions as described in each
+// individual source file, and distribute linked combinations
+// including the two.
+//
+// You must obey the GNU General Public License in all respects for
+// all of the code used other than OpenSSL.  If you modify file(s)
+// with this exception, you may extend this exception to your version
+// of the file(s), but you are not obligated to do so.  If you do not
+// wish to do so, delete this exception statement from your version.
+// If you delete this exception statement from all source files in the
+// program, then also delete it here.
+//
+// Contact:  Jari Sundell <jaris@ifi.uio.no>
+//
+//           Skomakerveien 33
+//           3185 Skoppum, NORWAY
+
+#include "config.h"
+
+#include <rak/timer.h>
+
+#include "thread_disk.h"
+
+#include "torrent/exceptions.h"
+#include "torrent/poll.h"
+#include "torrent/utils/log.h"
+
+namespace torrent {
+
+void
+thread_disk::init_thread() {
+  if (!Poll::slot_create_poll())
+    throw internal_error("thread_disk::init_thread(): Poll::slot_create_poll() not valid.");
+
+  m_poll = Poll::slot_create_poll()();
+  m_state = STATE_INITIALIZED;
+}
+
+void
+thread_disk::call_events() {
+  lt_log_print_locked(torrent::LOG_THREAD_NOTICE, "Got thread_disk tick.");
+}
+
+int64_t
+thread_disk::next_timeout_usec() {
+  return rak::timer::from_seconds(10).usec();
+}
+
+}
