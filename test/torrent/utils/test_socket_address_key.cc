c02721df1bf7b370bb34355607a34ff1c4b98c22
rakshasa <sundell.software@gmail.com>
2014-09-18 15:35:27 +0900

    Added test classes for socket_address_key.

---
+++ test/torrent/utils/test_socket_address_key.cc
@@ -0,0 +1,16 @@
+#include "config.h"
+
+#include "test_socket_address_key.h"
+
+#include "utils/instrumentation.h"
+#include "utils/queue_buckets.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TestSocketAddressKey);
+
+//
+// Basic tests:
+//
+
+void
+TestSocketAddressKey::test_basic() {
+}
