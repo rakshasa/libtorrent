d20ec47e6847509c61d1c052078fb8e33701aa2a
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2016-11-22 00:26:18 -0800

    de-indent udnsevent.cc (namespaces are not indented)

--- src/utils/udnsevent.cc
+++ src/utils/udnsevent.cc
@@ -13,202 +13,202 @@
 
 namespace torrent {
 
-  int udnserror_to_gaierror(int udnserror) {
-    switch (udnserror) {
-      case DNS_E_TEMPFAIL:
-        return EAI_AGAIN;
-      case DNS_E_PROTOCOL:
-        // this isn't quite right
-        return EAI_FAIL;
-      case DNS_E_NXDOMAIN:
-        return EAI_NONAME;
-      case DNS_E_NODATA:
-        return EAI_ADDRFAMILY;
-      case DNS_E_NOMEM:
-        return EAI_MEMORY;
-      case DNS_E_BADQUERY:
-        return EAI_NONAME;
-      default:
-        return EAI_ADDRFAMILY;
-    }
+int udnserror_to_gaierror(int udnserror) {
+  switch (udnserror) {
+    case DNS_E_TEMPFAIL:
+      return EAI_AGAIN;
+    case DNS_E_PROTOCOL:
+      // this isn't quite right
+      return EAI_FAIL;
+    case DNS_E_NXDOMAIN:
+      return EAI_NONAME;
+    case DNS_E_NODATA:
+      return EAI_ADDRFAMILY;
+    case DNS_E_NOMEM:
+      return EAI_MEMORY;
+    case DNS_E_BADQUERY:
+      return EAI_NONAME;
+    default:
+      return EAI_ADDRFAMILY;
   }
+}
 
-  /** Compatibility layers so udns can call std::function callbacks. */
+/** Compatibility layers so udns can call std::function callbacks. */
 
-  void a4_callback_wrapper(struct ::dns_ctx *ctx, ::dns_rr_a4 *result, void *data) {
-    struct sockaddr_in sa;
-    const sockaddr *sa_addr = (sockaddr *) &sa;
-    UdnsQuery *query = (UdnsQuery *) data;
-    // udns will free the a4_query after this callback exits
-    query->a4_query = NULL;
+void a4_callback_wrapper(struct ::dns_ctx *ctx, ::dns_rr_a4 *result, void *data) {
+  struct sockaddr_in sa;
+  const sockaddr *sa_addr = (sockaddr *) &sa;
+  UdnsQuery *query = (UdnsQuery *) data;
+  // udns will free the a4_query after this callback exits
+  query->a4_query = NULL;
 
-    if (result == NULL || result->dnsa4_nrr == 0) {
-      if (query->a6_query == NULL) {
-        // nothing more to do: call the callback with a failure status
-        (*(query->callback))(NULL, udnserror_to_gaierror(::dns_status(ctx)));
-        delete query;
-      }
-      // else: return and wait to see if we get an a6 response
-    } else {
-      sa.sin_family = AF_INET;
-      sa.sin_port = 0;
-      sa.sin_addr = result->dnsa4_addr[0];
-      if (query->a6_query != NULL) {
-        ::dns_cancel(ctx, query->a6_query);
-      }
-      (*query->callback)(sa_addr, 0);
+  if (result == NULL || result->dnsa4_nrr == 0) {
+    if (query->a6_query == NULL) {
+      // nothing more to do: call the callback with a failure status
+      (*(query->callback))(NULL, udnserror_to_gaierror(::dns_status(ctx)));
       delete query;
     }
-  }
-
-  void a6_callback_wrapper(struct ::dns_ctx *ctx, ::dns_rr_a6 *result, void *data) {
-    struct sockaddr_in6 sa;
-    const sockaddr *sa_addr = (sockaddr *) &sa;
-    UdnsQuery *query = (UdnsQuery *) data;
-    // udns will free the a6_query after this callback exits
-    query->a6_query = NULL;
-
-    if (result == NULL || result->dnsa6_nrr == 0) {
-      if (query->a4_query == NULL) {
-        // nothing more to do: call the callback with a failure status
-        (*(query->callback))(NULL, udnserror_to_gaierror(::dns_status(ctx)));
-        delete query;
-      }
-      // else: return and wait to see if we get an a6 response
-    } else {
-      sa.sin6_family = AF_INET6;
-      sa.sin6_port = 0;
-      sa.sin6_addr = result->dnsa6_addr[0];
-      if (query->a4_query != NULL) {
-        ::dns_cancel(ctx, query->a4_query);
-      }
-      (*query->callback)(sa_addr, 0);
-      delete query;
-    }
-  }
-
-
-  UdnsEvent::UdnsEvent() {
-    // reinitialize the default context, no-op
-    // TODO don't do this here --- do it once in the manager, or in rtorrent
-    ::dns_init(NULL, 0);
-    // thread-safe context isolated to this object:
-    m_ctx = ::dns_new(NULL);
-    int fd = ::dns_init(m_ctx, 1);
-    if (fd > 0) {
-      m_fileDesc = fd;
-    } else {
-      throw internal_error("dns_init failed");
-    }
-
-    m_taskTimeout.slot() = std::bind(&UdnsEvent::process_timeouts, this);
-  }
-
-  UdnsEvent::~UdnsEvent() {
-    priority_queue_erase(&taskScheduler, &m_taskTimeout);
-    ::dns_close(m_ctx);
-    ::dns_free(m_ctx);
-
-    for (auto it = std::begin(m_malformed_queries); it != std::end(m_malformed_queries); ++it) {
-      delete *it;
-    }
-  }
-
-  void UdnsEvent::event_read() {
-    ::dns_ioevent(m_ctx, 0);
-  }
-
-  void UdnsEvent::event_write() {
-  }
-
-  void UdnsEvent::event_error() {
-  }
-
-  struct UdnsQuery *UdnsEvent::enqueue_resolve(const char *name, int family, resolver_callback *callback) {
-    struct UdnsQuery *query = new UdnsQuery { NULL, NULL, callback, 0 };
-
-    if (family == AF_INET || family == AF_UNSPEC) {
-      query->a4_query = ::dns_submit_a4(m_ctx, name, 0, a4_callback_wrapper, query);
-      if (query->a4_query == NULL) {
-        // XXX udns does query parsing up front and will fail immediately
-        // during submission of malformed domain names, e.g., `..`. In order to
-        // maintain a clean interface, keep track of this query internally
-        // so we can call the callback later with a failure code
-        if (::dns_status(m_ctx) == DNS_E_BADQUERY) {
-          // this is what getaddrinfo(3) would return:
-          query->error = EAI_NONAME;
-          m_malformed_queries.push_back(query);
-          return query;
-        } else {
-          // unrecoverable errors, like ENOMEM
-          throw new internal_error("dns_submit_a4 failed");
-        }
-      }
-    }
-
-    if (family == AF_INET6 || family == AF_UNSPEC) {
-      query->a6_query = ::dns_submit_a6(m_ctx, name, 0, a6_callback_wrapper, query);
-      if (query->a6_query == NULL) {
-        // it should be impossible for dns_submit_a6 to fail if dns_submit_a4
-        // succeeded, but just in case, make it a hard failure:
-        if (::dns_status(m_ctx) == DNS_E_BADQUERY && query->a4_query == NULL) {
-          query->error = EAI_NONAME;
-          m_malformed_queries.push_back(query);
-          return query;
-        } else {
-          throw new internal_error("dns_submit_a6 failed");
-        }
-      }
-    }
-
-    return query;
-  }
-
-  void UdnsEvent::flush_resolves() {
-    // first process any queries that were malformed
-    while (!m_malformed_queries.empty()) {
-      UdnsQuery *query = m_malformed_queries.front();
-      m_malformed_queries.pop_front();
-      (*(query->callback))(NULL, query->error);
-      delete query;
-    }
-    process_timeouts();
-  }
-
-  void UdnsEvent::process_timeouts() {
-    int timeout = ::dns_timeouts(m_ctx, -1, 0);
-    if (timeout != -1) {
-      manager->poll()->insert_read(this);
-      manager->poll()->insert_error(this);
-      priority_queue_erase(&taskScheduler, &m_taskTimeout);
-      priority_queue_insert(&taskScheduler, &m_taskTimeout, (cachedTime + rak::timer::from_seconds(timeout)).round_seconds());
-    } else {
-      // no pending queries
-      manager->poll()->remove_read(this);
-      manager->poll()->remove_error(this);
-    }
-  }
-
-  void UdnsEvent::cancel(struct UdnsQuery *query) {
-    if (query == NULL) {
-      return;
-    }
-    if (query->a4_query != NULL) {
-      ::dns_cancel(m_ctx, query->a4_query);
-    }
+    // else: return and wait to see if we get an a6 response
+  } else {
+    sa.sin_family = AF_INET;
+    sa.sin_port = 0;
+    sa.sin_addr = result->dnsa4_addr[0];
     if (query->a6_query != NULL) {
-      ::dns_cancel(m_ctx, query->a6_query);
+      ::dns_cancel(ctx, query->a6_query);
     }
-    auto it = std::find(std::begin(m_malformed_queries), std::end(m_malformed_queries), query);
-    if (it != std::end(m_malformed_queries)) {
-      m_malformed_queries.erase(it);
-    }
+    (*query->callback)(sa_addr, 0);
     delete query;
   }
+}
 
-  const char *UdnsEvent::type_name() {
-    return "UdnsEvent";
+void a6_callback_wrapper(struct ::dns_ctx *ctx, ::dns_rr_a6 *result, void *data) {
+  struct sockaddr_in6 sa;
+  const sockaddr *sa_addr = (sockaddr *) &sa;
+  UdnsQuery *query = (UdnsQuery *) data;
+  // udns will free the a6_query after this callback exits
+  query->a6_query = NULL;
+
+  if (result == NULL || result->dnsa6_nrr == 0) {
+    if (query->a4_query == NULL) {
+      // nothing more to do: call the callback with a failure status
+      (*(query->callback))(NULL, udnserror_to_gaierror(::dns_status(ctx)));
+      delete query;
+    }
+    // else: return and wait to see if we get an a6 response
+  } else {
+    sa.sin6_family = AF_INET6;
+    sa.sin6_port = 0;
+    sa.sin6_addr = result->dnsa6_addr[0];
+    if (query->a4_query != NULL) {
+      ::dns_cancel(ctx, query->a4_query);
+    }
+    (*query->callback)(sa_addr, 0);
+    delete query;
   }
+}
+
+
+UdnsEvent::UdnsEvent() {
+  // reinitialize the default context, no-op
+  // TODO don't do this here --- do it once in the manager, or in rtorrent
+  ::dns_init(NULL, 0);
+  // thread-safe context isolated to this object:
+  m_ctx = ::dns_new(NULL);
+  int fd = ::dns_init(m_ctx, 1);
+  if (fd > 0) {
+    m_fileDesc = fd;
+  } else {
+    throw internal_error("dns_init failed");
+  }
+
+  m_taskTimeout.slot() = std::bind(&UdnsEvent::process_timeouts, this);
+}
+
+UdnsEvent::~UdnsEvent() {
+  priority_queue_erase(&taskScheduler, &m_taskTimeout);
+  ::dns_close(m_ctx);
+  ::dns_free(m_ctx);
+
+  for (auto it = std::begin(m_malformed_queries); it != std::end(m_malformed_queries); ++it) {
+    delete *it;
+  }
+}
+
+void UdnsEvent::event_read() {
+  ::dns_ioevent(m_ctx, 0);
+}
+
+void UdnsEvent::event_write() {
+}
+
+void UdnsEvent::event_error() {
+}
+
+struct UdnsQuery *UdnsEvent::enqueue_resolve(const char *name, int family, resolver_callback *callback) {
+  struct UdnsQuery *query = new UdnsQuery { NULL, NULL, callback, 0 };
+
+  if (family == AF_INET || family == AF_UNSPEC) {
+    query->a4_query = ::dns_submit_a4(m_ctx, name, 0, a4_callback_wrapper, query);
+    if (query->a4_query == NULL) {
+      // XXX udns does query parsing up front and will fail immediately
+      // during submission of malformed domain names, e.g., `..`. In order to
+      // maintain a clean interface, keep track of this query internally
+      // so we can call the callback later with a failure code
+      if (::dns_status(m_ctx) == DNS_E_BADQUERY) {
+        // this is what getaddrinfo(3) would return:
+        query->error = EAI_NONAME;
+        m_malformed_queries.push_back(query);
+        return query;
+      } else {
+        // unrecoverable errors, like ENOMEM
+        throw new internal_error("dns_submit_a4 failed");
+      }
+    }
+  }
+
+  if (family == AF_INET6 || family == AF_UNSPEC) {
+    query->a6_query = ::dns_submit_a6(m_ctx, name, 0, a6_callback_wrapper, query);
+    if (query->a6_query == NULL) {
+      // it should be impossible for dns_submit_a6 to fail if dns_submit_a4
+      // succeeded, but just in case, make it a hard failure:
+      if (::dns_status(m_ctx) == DNS_E_BADQUERY && query->a4_query == NULL) {
+        query->error = EAI_NONAME;
+        m_malformed_queries.push_back(query);
+        return query;
+      } else {
+        throw new internal_error("dns_submit_a6 failed");
+      }
+    }
+  }
+
+  return query;
+}
+
+void UdnsEvent::flush_resolves() {
+  // first process any queries that were malformed
+  while (!m_malformed_queries.empty()) {
+    UdnsQuery *query = m_malformed_queries.front();
+    m_malformed_queries.pop_front();
+    (*(query->callback))(NULL, query->error);
+    delete query;
+  }
+  process_timeouts();
+}
+
+void UdnsEvent::process_timeouts() {
+  int timeout = ::dns_timeouts(m_ctx, -1, 0);
+  if (timeout != -1) {
+    manager->poll()->insert_read(this);
+    manager->poll()->insert_error(this);
+    priority_queue_erase(&taskScheduler, &m_taskTimeout);
+    priority_queue_insert(&taskScheduler, &m_taskTimeout, (cachedTime + rak::timer::from_seconds(timeout)).round_seconds());
+  } else {
+    // no pending queries
+    manager->poll()->remove_read(this);
+    manager->poll()->remove_error(this);
+  }
+}
+
+void UdnsEvent::cancel(struct UdnsQuery *query) {
+  if (query == NULL) {
+    return;
+  }
+  if (query->a4_query != NULL) {
+    ::dns_cancel(m_ctx, query->a4_query);
+  }
+  if (query->a6_query != NULL) {
+    ::dns_cancel(m_ctx, query->a6_query);
+  }
+  auto it = std::find(std::begin(m_malformed_queries), std::end(m_malformed_queries), query);
+  if (it != std::end(m_malformed_queries)) {
+    m_malformed_queries.erase(it);
+  }
+  delete query;
+}
+
+const char *UdnsEvent::type_name() {
+  return "UdnsEvent";
+}
 
 }
 
