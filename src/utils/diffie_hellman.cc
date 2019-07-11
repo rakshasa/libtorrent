12e62f70b3b9dbfcefae23a47f7e69cab3567102
rakshasa <sundell.software@gmail.com>
2019-07-12 00:29:35 +0200

    Fixed diffie hellman implementation.

--- src/utils/diffie_hellman.cc
+++ src/utils/diffie_hellman.cc
@@ -1,118 +1,79 @@
-// libTorrent - BitTorrent library
-// Copyright (C) 2005-2011, Jari Sundell
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-// In addition, as a special exception, the copyright holders give
-// permission to link the code of portions of this program with the
-// OpenSSL library under certain conditions as described in each
-// individual source file, and distribute linked combinations
-// including the two.
-//
-// You must obey the GNU General Public License in all respects for
-// all of the code used other than OpenSSL.  If you modify file(s)
-// with this exception, you may extend this exception to your version
-// of the file(s), but you are not obligated to do so.  If you do not
-// wish to do so, delete this exception statement from your version.
-// If you delete this exception statement from all source files in the
-// program, then also delete it here.
-//
-// Contact:  Jari Sundell <jaris@ifi.uio.no>
-//
-//           Skomakerveien 33
-//           3185 Skoppum, NORWAY
-
 #include "config.h"
 
+#include "diffie_hellman.h"
+
+#include "torrent/exceptions.h"
+
 #include <cstring>
-#include <string>
 
 #ifdef USE_OPENSSL
+#include <openssl/dh.h>
 #include <openssl/bn.h>
 #endif
 
-#include "diffie_hellman.h"
-#include "torrent/exceptions.h"
-
 namespace torrent {
+
+#ifdef USE_OPENSSL
+
+static void      dh_free(void* dh) { DH_free(reinterpret_cast<DH*>(dh)); }
+static DiffieHellman::dh_ptr dh_new() { return DiffieHellman::dh_ptr(reinterpret_cast<void*>(DH_new()), &dh_free); }
+static DH*       dh_get(DiffieHellman::dh_ptr& dh) { return reinterpret_cast<DH*>(dh.get()); }
+
+static bool
+dh_set_pg(DiffieHellman::dh_ptr& dh, BIGNUM* dh_p, BIGNUM* dh_g) {
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+  return DH_set0_pqg(reinterpret_cast<DH*>(dh.get()), dh_p, nullptr, dh_g);
+#else
+  reinterpret_cast<DH*>(dh.get())->p = dh_p;
+  reinterpret_cast<DH*>(dh.get())->g = dh_g;
+  return true;
+#endif
+}
+
+static const BIGNUM* dh_get_pub_key(const DiffieHellman::dh_ptr& dh) {
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+  const BIGNUM *pub_key;
+  DH_get0_key(reinterpret_cast<DH*>(dh.get()), &pub_key, nullptr);
+  return pub_key;
+#else
+  return dh != nullptr ? reinterpret_cast<DH*>(dh.get())->pub_key : nullptr;
+#endif
+}
+
+#else
+static DiffieHellman::dh_ptr dh_new() { throw internal_error("Compiled without encryption support."); }
+static void  dh_free(void* dh) {}
+static void* dh_get_pub_key(const DiffieHellman::dh_ptr& dh) { return nullptr; }
+#endif
 
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 DiffieHellman::DiffieHellman(const unsigned char *prime, int primeLength,
                              const unsigned char *generator, int generatorLength) :
-  m_secret(NULL), m_size(0) {
+  m_dh(dh_new()), m_size(0) {
 
 #ifdef USE_OPENSSL
+  BIGNUM* dh_p = BN_bin2bn(prime, primeLength, nullptr);
+  BIGNUM* dh_g = BN_bin2bn(generator, generatorLength, nullptr);
 
-  m_dh = DH_new();
+  if (dh_p == nullptr || dh_g == nullptr || !dh_set_pg(m_dh, dh_p, dh_g))
+    throw internal_error("Could not generate Diffie-Hellman parameters");
 
-#ifdef USE_OPENSSL_1_1
-  BIGNUM * const dh_p = BN_bin2bn(prime, primeLength, NULL);
-  BIGNUM * const dh_g = BN_bin2bn(generator, generatorLength, NULL);
-
-  if (dh_p == NULL || dh_g == NULL ||
-      !DH_set0_pqg(m_dh, dh_p, NULL, dh_g))
-	  throw internal_error("Could not generate Diffie-Hellman parameters");
-#else
-  m_dh->p = BN_bin2bn(prime, primeLength, NULL);
-  m_dh->g = BN_bin2bn(generator, generatorLength, NULL);
-#endif
-
-  DH_generate_key(m_dh);
-
-#else
-  throw internal_error("Compiled without encryption support.");
-#endif
-};
-
-DiffieHellman::~DiffieHellman() {
-  delete [] m_secret;
-#ifdef USE_OPENSSL
-  DH_free(m_dh);
+  DH_generate_key(dh_get(m_dh));
 #endif
 };
 
 bool
 DiffieHellman::is_valid() const {
-#ifdef USE_OPENSSL
-  if (m_dh == NULL)
-    return false;
-
-#ifdef USE_OPENSSL_1_1
-  const BIGNUM *pub_key;
-
-  DH_get0_key(m_dh, &pub_key, NULL);
-
-  return pub_key != NULL;
-#else
-  return m_dh != NULL && m_dh->pub_key != NULL;
-#endif
-
-#else
-  return false;
-#endif
+  return dh_get_pub_key(m_dh) != nullptr;
 }
 
 bool
 DiffieHellman::compute_secret(const unsigned char *pubkey, unsigned int length) {
 #ifdef USE_OPENSSL
-  BIGNUM* k = BN_bin2bn(pubkey, length, NULL);
+  BIGNUM* k = BN_bin2bn(pubkey, length, nullptr);
 
-  delete [] m_secret;
-  m_secret = new char[DH_size(m_dh)];
-
-  m_size = DH_compute_key((unsigned char*)m_secret, k, m_dh);
+  m_secret.reset(new char[DH_size(dh_get(m_dh))]);
+  m_size = DH_compute_key(reinterpret_cast<unsigned char*>(m_secret.get()), k, dh_get(m_dh));
   
   BN_free(k);
 
@@ -124,16 +85,10 @@ DiffieHellman::compute_secret(const unsigned char *pubkey, unsigned int length) 
 
 void
 DiffieHellman::store_pub_key(unsigned char* dest, unsigned int length) {
-#ifdef USE_OPENSSL
   std::memset(dest, 0, length);
 
-  const BIGNUM *pub_key;
-
-#ifdef USE_OPENSSL_1_1
-  DH_get0_key(m_dh, &pub_key, NULL);
-#else
-  pub_key = m_dh->pub_key;
-#endif
+#ifdef USE_OPENSSL
+  const BIGNUM *pub_key = dh_get_pub_key(m_dh);
 
   if ((int)length >= BN_num_bytes(pub_key))
     BN_bn2bin(pub_key, dest + length - BN_num_bytes(pub_key));
--- src/utils/diffie_hellman.h
+++ src/utils/diffie_hellman.h
@@ -1,79 +1,38 @@
-// libTorrent - BitTorrent library
-// Copyright (C) 2005-2011, Jari Sundell
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-// In addition, as a special exception, the copyright holders give
-// permission to link the code of portions of this program with the
-// OpenSSL library under certain conditions as described in each
-// individual source file, and distribute linked combinations
-// including the two.
-//
-// You must obey the GNU General Public License in all respects for
-// all of the code used other than OpenSSL.  If you modify file(s)
-// with this exception, you may extend this exception to your version
-// of the file(s), but you are not obligated to do so.  If you do not
-// wish to do so, delete this exception statement from your version.
-// If you delete this exception statement from all source files in the
-// program, then also delete it here.
-//
-// Contact:  Jari Sundell <jaris@ifi.uio.no>
-//
-//           Skomakerveien 33
-//           3185 Skoppum, NORWAY
-
 #ifndef LIBTORRENT_DIFFIE_HELLMAN_H
 #define LIBTORRENT_DIFFIE_HELLMAN_H
 
 #include "config.h"
 
+#include <memory>
 #include <string>
-
-#ifdef USE_OPENSSL
-#include <openssl/dh.h>
-#endif
 
 namespace torrent {
 
 class DiffieHellman {
 public:
+  typedef std::unique_ptr<char[]> secret_ptr;
+  typedef std::unique_ptr<void, void (*)(void*)> dh_ptr;
+
   DiffieHellman(const unsigned char prime[], int primeLength,
                 const unsigned char generator[], int generatorLength);
-  ~DiffieHellman();
 
-  bool                compute_secret(const unsigned char pubkey[], unsigned int length);
-  void                store_pub_key(unsigned char* dest, unsigned int length);
+  bool         is_valid() const;
 
-  bool                is_valid() const;
+  bool         compute_secret(const unsigned char pubkey[], unsigned int length);
+  void         store_pub_key(unsigned char* dest, unsigned int length);
 
-  unsigned int        size() const         { return m_size; }
+  unsigned int size() const         { return m_size; }
 
-  const char*         c_str() const        { return m_secret; }
-  std::string         secret_str() const   { return std::string(m_secret, m_size); }
+  const char*  c_str() const        { return m_secret.get(); }
+  std::string  secret_str() const   { return std::string(m_secret.get(), m_size); }
 
 private:
   DiffieHellman(const DiffieHellman& dh);
   DiffieHellman& operator = (const DiffieHellman& dh);
 
-#ifdef USE_OPENSSL
-  DH*                 m_dh;
-#else
-  void*               m_void;
-#endif
-  char*               m_secret;
-  unsigned int        m_size;
+  dh_ptr       m_dh;
+  secret_ptr   m_secret;
+  int          m_size;
 };
 
 };
