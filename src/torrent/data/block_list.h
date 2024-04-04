dfad433c8a2a8bd2a9775f786be1af7627a2a495
kannibalox <kannibalox@gmail.com>
2024-04-03 21:06:53 -0400

    Speed up delegating by returning multiple request assignments at once

--- src/torrent/data/block_list.h
+++ src/torrent/data/block_list.h
@@ -1,3 +1,4 @@
+
 // libTorrent - BitTorrent library
 // Copyright (C) 2005-2011, Jari Sundell
 //
@@ -44,70 +45,27 @@
 
 namespace torrent {
 
-// Temporary workaround until we can use C++11's std::vector::emblace_back.
-template <typename Type>
-class no_copy_vector {
+class LIBTORRENT_EXPORT BlockList : private std::vector<Block> {
 public:
-  typedef Type        value_type;
-  typedef size_t      size_type;
-  typedef value_type& reference;
-  typedef ptrdiff_t   difference_type;
-
-  typedef value_type*       iterator;
-  typedef const value_type* const_iterator;
-
-  no_copy_vector() : m_size(0), m_values(NULL) {}
-  ~no_copy_vector() { clear(); }
-
-  size_type size() const { return m_size; }
-  bool      empty() const { return m_size == 0; }
-
-  void resize(size_type s) { clear(); m_size = s; m_values = new value_type[s]; }
-
-  void clear() { delete [] m_values; m_values = NULL; m_size = 0; }
-
-  iterator       begin() { return m_values; }
-  const_iterator begin() const { return m_values; }
-
-  iterator       end() { return m_values + m_size; }
-  const_iterator end() const { return m_values + m_size; }
-
-  value_type&    back() { return *(m_values + m_size - 1); }
-
-  value_type&    operator[](size_type idx) { return m_values[idx]; }
-
-private:
-  no_copy_vector(const no_copy_vector&);
-  void operator = (const no_copy_vector&);
-
-  size_type m_size;
-  Block*    m_values;
-};
-
-class LIBTORRENT_EXPORT BlockList : public no_copy_vector<Block> {
-public:
-  typedef no_copy_vector<Block> base_type;
-  typedef uint32_t              size_type;
+  typedef uint32_t           size_type;
+  typedef std::vector<Block> base_type;
 
   using base_type::value_type;
   using base_type::reference;
   using base_type::difference_type;
 
   using base_type::iterator;
-  // using base_type::reverse_iterator;
+  using base_type::const_iterator;
   using base_type::size;
   using base_type::empty;
 
   using base_type::begin;
   using base_type::end;
-  // using base_type::rbegin;
-  // using base_type::rend;
 
   using base_type::operator[];
 
   BlockList(const Piece& piece, uint32_t blockLength);
   ~BlockList();
-
   bool                is_all_finished() const       { return m_finished == size(); }
 
   const Piece&        piece() const                 { return m_piece; }
