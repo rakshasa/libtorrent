ec0041e69ee97efd934365298ce7f739389d62e1
kannibalox <kannibalox@gmail.com>
2024-04-03 21:06:53 -0400

    Speed up delegating by returning multiple request assignments at once

--- src/protocol/request_list.cc
+++ src/protocol/request_list.cc
@@ -88,19 +88,26 @@ RequestList::~RequestList() {
   priority_queue_erase(&taskScheduler, &m_delay_process_unordered);
 }
 
-const Piece*
-RequestList::delegate() {
-  BlockTransfer* transfer = m_delegator->delegate(m_peerChunks, m_affinity);
+std::vector<const Piece*>
+RequestList::delegate(uint32_t maxPieces) {
+  std::vector<BlockTransfer*> transfers = m_delegator->delegate(m_peerChunks, m_affinity, maxPieces);
 
-  instrumentation_update(INSTRUMENTATION_TRANSFER_REQUESTS_DELEGATED, 1);
+  std::vector<const Piece*> pieces;
 
-  if (transfer == NULL)
-    return NULL;
+  if (transfers.empty())
+    return pieces;
 
-  m_affinity = transfer->index();
-  m_queues.push_back(bucket_queued, transfer);
+  instrumentation_update(INSTRUMENTATION_TRANSFER_REQUESTS_DELEGATED, transfers.size());
 
-  return &transfer->piece();
+  for (auto& itr : transfers) {
+    m_queues.push_back(bucket_queued, itr);
+    pieces.push_back(&(itr->piece()));
+  }
+
+  // Use the last index returned for the next affinity
+  m_affinity = transfers.back()->index();
+
+  return pieces;
 }
 
 void
