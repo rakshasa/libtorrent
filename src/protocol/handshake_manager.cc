f998282358bd92f23e9fe59bcb62befc8dee3033
kannibalox <kannibalox@gmail.com>
2024-11-30 12:13:37 -0500

    Convert more of functional.h to standard library constructs

--- src/protocol/handshake_manager.cc
+++ src/protocol/handshake_manager.cc
@@ -73,8 +73,9 @@ HandshakeManager::find(const rak::socket_address& sa) {
 
 void
 HandshakeManager::erase_download(DownloadMain* info) {
-  iterator split = std::partition(base_type::begin(), base_type::end(),
-                                  rak::not_equal(info, std::mem_fun(&Handshake::download)));
+  auto split = std::partition(base_type::begin(), base_type::end(), [info](Handshake* h) {
+    return info != h->download();
+  });
 
   std::for_each(split, base_type::end(), std::ptr_fun(&handshake_manager_delete_handshake));
   base_type::erase(split, base_type::end());
