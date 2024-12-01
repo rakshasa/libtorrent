rakshasa/libtorrent

commit eddcb1f59105bd0ad2377b477d58641a1c6c9f67

Minor cleanup.

src/protocol/handshake.cc
@@ -25,7 +25,7 @@
   lt_log_print(LOG_CONNECTION_HANDSHAKE, "handshake->%s: " log_fmt,     \
                m_address.pretty_address_str().c_str(), __VA_ARGS__);
 
-#if USE_EXTRA_DEBUG
+#if DISABLED__USE_EXTRA_DEBUG
 #define LT_LOG_EXTRA_DEBUG_SA(sa, log_fmt, ...)                         \
   lt_log_print(LOG_CONNECTION_HANDSHAKE, "handshake->%s: " log_fmt, sap_pretty_str(m_address).c_str(), __VA_ARGS__);
 #else

rak/priority_queue.h
@@ -49,7 +49,7 @@ public:
 
   template <typename Key>
   iterator find(const Key& key) {
-    return std::find_if(begin(), end(), [=](auto& value) { return m_equal(value, key); });
+    return std::find_if(begin(), end(), [&](auto& value) { return m_equal(value, key); });
   }
 
   template <typename Key>
@@ -73,36 +73,6 @@ private:
   Compare             m_compare;
   Equal               m_equal;
 };
-
-// Iterate while the top node has higher priority, as 'Compare'
-// returns false.
-template <typename Queue, typename Compare>
-class queue_pop_iterator
-  : public std::iterator<std::forward_iterator_tag, void, void, void, void> {
-public:
-  typedef Queue container_type;
-
-  queue_pop_iterator() : m_queue(NULL) {}
-  queue_pop_iterator(Queue* q, Compare c) : m_queue(q), m_compare(c) {}
-
-  queue_pop_iterator& operator ++ ()                     { m_queue->pop(); return *this; }
-  queue_pop_iterator& operator ++ (int)                  { m_queue->pop(); return *this; }
-
-  typename container_type::const_reference operator * () { return m_queue->top(); }
-
-  bool operator != (const queue_pop_iterator& itr)       { return !m_queue->empty() && !m_compare(m_queue->top()); }
-  bool operator == (const queue_pop_iterator& itr)       { return m_queue->empty() || m_compare(m_queue->top()); }
-
-private:
-  Queue*  m_queue;
-  Compare m_compare;
-};
-
-template <typename Queue, typename Compare>
-inline queue_pop_iterator<Queue, Compare>
-queue_popper(Queue& queue, Compare comp) {
-  return queue_pop_iterator<Queue, Compare>(&queue, comp);
-}
 
 }
 

src/data/chunk.h
@@ -85,7 +85,7 @@ Chunk::at_position(uint32_t pos, iterator itr) {
 
 inline Chunk::iterator
 Chunk::find_address(void* ptr) {
-  return std::find_if(begin(), end(), [=](auto& part) { return part.has_address(ptr); });
+  return std::find_if(begin(), end(), [&](auto& part) { return part.has_address(ptr); });
 }
 
 }

src/net/address_list.cc
@@ -8,7 +8,7 @@ namespace torrent {
 
 void
 AddressList::parse_address_normal(const Object::list_type& b) {
-  std::for_each(b.begin(), b.end(), [=](auto& obj) {
+  std::for_each(b.begin(), b.end(), [&](auto& obj) {
       if (!obj.is_map())
         return;
       if (!obj.has_key_string("ip"))

commit 75bbcf264c36449fcd89167ddb8b7b9c3d1cd230

fix buffer position tracking in read_port
This appears to be a bug introduced in #254 /
11c38cc540bc1ca7c193cdc28be6e9890df4c84b

src/protocol/handshake.cc
@@ -630,7 +630,7 @@ Handshake::read_port() {
   m_readBuffer.read_8();
 
   if (length == 2)
-    manager->dht_manager()->add_node(m_address.get(), m_readBuffer.read_16());
+    manager->dht_manager()->add_node(m_address.get(), m_readBuffer.peek_16());
 
   m_readBuffer.consume(length);
   return true;