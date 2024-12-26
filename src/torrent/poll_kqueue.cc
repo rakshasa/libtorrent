40c582dfc0c60aae2727c3fce1aed15b86c9fa0c
Rui Chen <rui@chenrui.dev>
2024-12-26 18:39:50 -0500

    fix: remove `rak::equal` and `rak::mem_ref` refs
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- src/torrent/poll_kqueue.cc
+++ src/torrent/poll_kqueue.cc
@@ -279,7 +279,7 @@ PollKQueue::close(Event* event) {
       itr->udata = NULL;
 
   m_changedEvents = std::remove_if(m_changes, m_changes + m_changedEvents,
-                                   rak::equal(event, rak::mem_ref(&kevent::udata))) - m_changes;
+                                   [event](const struct kevent& ke) { return ke.udata == event; }) - m_changes;
 }
 
 void
@@ -305,7 +305,7 @@ PollKQueue::closed(Event* event) {
       itr->udata = NULL;
 
   m_changedEvents = std::remove_if(m_changes, m_changes + m_changedEvents,
-                                   rak::equal(event, rak::mem_ref(&kevent::udata))) - m_changes;
+                                   [event](const struct kevent& ke) { return ke.udata == event; }) - m_changes;
 }
 
 // Use custom defines for EPOLL* to make the below code compile with
