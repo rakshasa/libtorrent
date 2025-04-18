eb7d2cb38a46db68800e03d98614c6ddb2b15e2e
Rosen Penev <rosenp@gmail.com>
2025-04-17 17:36:31 -0700

    private/protected destructors to public
    Found with cppcoreguidelines-virtual-class-destructor
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/data/thread_disk.h
+++ src/data/thread_disk.h
@@ -9,6 +9,7 @@ namespace torrent {
 
 class LIBTORRENT_EXPORT ThreadDisk : public utils::Thread {
 public:
+  ~ThreadDisk() override;
 
   static void        create_thread();
   static void        destroy_thread();
@@ -22,7 +23,6 @@ public:
 
 private:
   ThreadDisk() = default;
-  ~ThreadDisk() override;
 
   void                      call_events() override;
   std::chrono::microseconds next_timeout() override;
