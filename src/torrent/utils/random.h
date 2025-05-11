d51daa147a019ca10637f737f077cda3b7d4f880
rakshasa <sundell.software@gmail.com>
2025-05-11 14:24:44 +0200

    Stuff.

--- src/torrent/utils/random.h
+++ src/torrent/utils/random.h
@@ -1,14 +1,14 @@
 #ifndef LIBTORRENT_TORRENT_UTILS_RANDOM_H
 #define LIBTORRENT_TORRENT_UTILS_RANDOM_H
 
-#include <cinttypes>
 #include <limits>
 #include <random>
+#include <torrent/common.h>
 
 namespace torrent {
 
-[[gnu::weak]] [[gnu::visibility("default")]] uint16_t random_uniform_uint16(uint16_t min = std::numeric_limits<uint16_t>::min(), uint16_t max = std::numeric_limits<uint16_t>::max());
-[[gnu::weak]] [[gnu::visibility("default")]] uint32_t random_uniform_uint32(uint32_t min = std::numeric_limits<uint32_t>::min(), uint32_t max = std::numeric_limits<uint32_t>::max());
+[[gnu::weak]] LIBTORRENT_EXPORT uint16_t random_uniform_uint16(uint16_t min = std::numeric_limits<uint16_t>::min(), uint16_t max = std::numeric_limits<uint16_t>::max());
+[[gnu::weak]] LIBTORRENT_EXPORT uint32_t random_uniform_uint32(uint32_t min = std::numeric_limits<uint32_t>::min(), uint32_t max = std::numeric_limits<uint32_t>::max());
 
 }
 
