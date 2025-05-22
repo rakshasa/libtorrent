f997532adb39c10107c2ce39f9c9f52c45f55625
Rosen Penev <rosenp@gmail.com>
2025-05-22 12:56:55 -0700

    sha1: move back to deprecated API
    The EVP implementation here is leaking quite a bit of memory. Instead
    of dealing with heap allocation everywhere, move back to SHA_CTX.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/utils/sha1.h
+++ src/utils/sha1.h
@@ -2,16 +2,10 @@
 #define LIBTORRENT_HASH_COMPUTE_H
 
 #include <cstring>
-#include <memory>
-#include <openssl/evp.h>
+
+#include <openssl/sha.h>
 
 namespace torrent {
-
-struct sha1_deleter {
-  constexpr sha1_deleter() noexcept = default;
-
-  void operator()(const EVP_MD_CTX* ctx) const { EVP_MD_CTX_free(const_cast<EVP_MD_CTX*>(ctx)); }
-};
 
 class Sha1 {
 public:
@@ -21,25 +15,23 @@ public:
   void final_c(void* buffer);
 
 private:
-  std::unique_ptr<EVP_MD_CTX, sha1_deleter> m_ctx;
+  SHA_CTX m_ctx;
 };
 
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 inline void
 Sha1::init() {
-  if (m_ctx == nullptr)
-    m_ctx.reset(EVP_MD_CTX_new());
-
-  EVP_DigestInit_ex(m_ctx.get(), EVP_sha1(), nullptr);
+  SHA1_Init(&m_ctx);
 }
 
 inline void
 Sha1::update(const void* data, unsigned int length) {
-  EVP_DigestUpdate(m_ctx.get(), data, length);
+  SHA1_Update(&m_ctx, data, length);
 }
 
 inline void
 Sha1::final_c(void* buffer) {
-  EVP_DigestFinal_ex(m_ctx.get(), static_cast<unsigned char*>(buffer), nullptr);
+  SHA1_Final(static_cast<unsigned char*>(buffer), &m_ctx);
 }
 
 inline void
