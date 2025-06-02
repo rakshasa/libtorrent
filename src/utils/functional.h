715746cfdb849b42a9d041ad77bc83997846e2fe
Rosen Penev <rosenp@gmail.com>
2025-06-02 13:34:04 -0700

    clang-tidy: avoid use after forward
    Store the args and then use std::apply do avoid this.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/utils/functional.h
+++ src/utils/functional.h
@@ -1,6 +1,7 @@
 #ifndef LIBTORRENT_UTILS_FUNCTIONAL_H
 #define LIBTORRENT_UTILS_FUNCTIONAL_H
 
+#include <tuple>
 #include <utility>
 
 namespace utils {
@@ -15,12 +16,14 @@ slot_list_call(const Container& slot_list, Args&&... args) {
   auto first = slot_list.begin();
   auto next  = slot_list.begin();
 
+  auto args_tuple = std::forward_as_tuple(std::forward<Args>(args)...);
+
   while (++next != slot_list.end()) {
-    (*first)(std::forward<Args>(args)...);
+    std::apply(*first, args_tuple);
     first = next;
   }
 
-  (*first)(std::forward<Args>(args)...);
+  std::apply(*first, args_tuple);
 }
 
 } // namespace utils
