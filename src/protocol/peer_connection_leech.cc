af5b292bd29dde8ceb575ffd46508c8aa36c16d3
Rosen Penev <rosenp@gmail.com>
2025-06-13 15:47:15 -0700

    clang: add fallthrough attribute
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/protocol/peer_connection_leech.cc
+++ src/protocol/peer_connection_leech.cc
@@ -569,6 +569,7 @@ PeerConnection<type>::event_write() {
 
         m_up->set_state(ProtocolWrite::MSG);
 
+	[[fallthrough]];
       case ProtocolWrite::MSG:
         if (!m_up->buffer()->consume(m_up->throttle()->node_used_unthrottled(write_stream_throws(m_up->buffer()->position(), m_up->buffer()->remaining()))))
           return;
@@ -593,6 +594,7 @@ PeerConnection<type>::event_write() {
           break;
         }
 
+	[[fallthrough]];
       case ProtocolWrite::WRITE_PIECE:
         if (!up_chunk())
           return;
