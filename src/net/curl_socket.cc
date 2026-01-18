2214198f45067bc12937304d8816b715df188029
rakshasa <sundell.software@gmail.com>
2026-01-18 12:38:26 +0100

    Stuff.

--- src/net/curl_socket.cc
+++ src/net/curl_socket.cc
@@ -11,16 +11,19 @@
 #include "torrent/net/poll.h"
 #include "torrent/utils/log.h"
 
-#define LT_LOG_DEBUG_THIS(log_fmt, ...) \
+#define LT_LOG_DEBUG_THIS(log_fmt, ...)                                 \
   lt_log_print(LOG_CONNECTION_FD, "curl_socket->%p(%i): " log_fmt, this, this->m_fileDesc, __VA_ARGS__);
 
 //#define LT_LOG_DEBUG_SOCKET(log_fmt, ...)
-#define LT_LOG_DEBUG_SOCKET(log_fmt, ...) \
+#define LT_LOG_DEBUG_SOCKET(log_fmt, ...)                               \
   lt_log_print(LOG_CONNECTION_FD, "curl_socket->%p(%i): " log_fmt, socket, socket != nullptr ? socket->m_fileDesc : 0, __VA_ARGS__);
 
-// #define LT_LOG_DEBUG_SOCKET_FD(log_fmt, ...)
 #define LT_LOG_DEBUG_SOCKET_FD(log_fmt, ...)                            \
   lt_log_print(LOG_CONNECTION_FD, "curl_socket->%p(%i): fd:%i : " log_fmt, socket, socket != nullptr ? socket->m_fileDesc : 0, fd, __VA_ARGS__);
+
+// #define LT_LOG_DEBUG_SOCKET_FD(log_fmt, ...)
+#define LT_LOG_DEBUG_SOCKET_FD_HANDLE(log_fmt, ...)                            \
+  lt_log_print(LOG_CONNECTION_FD, "curl_socket->%p(%i): fd:%i easy_handle:%p : " log_fmt, socket, socket != nullptr ? socket->m_fileDesc : 0, fd, easy_handle, __VA_ARGS__);
 
 namespace torrent::net {
 
@@ -36,6 +39,10 @@ CurlSocket::~CurlSocket() {
   m_self_exists = false;
 }
 
+// TODO: Rewrite to only use a single CurlSocket object for each fd, and update its state.
+//
+// This will allow us to listen for socket error events on idle connections.
+
 int
 CurlSocket::receive_socket(CURL* easy_handle, curl_socket_t fd, int what, CurlStack* stack, CurlSocket* socket) {
   // We always return 0, even when stack is not running, as we depend on these calls to delete
@@ -43,7 +50,7 @@ CurlSocket::receive_socket(CURL* easy_handle, curl_socket_t fd, int what, CurlSt
 
   if (socket != nullptr) {
     if (!socket->m_self_exists) {
-      LT_LOG_DEBUG_SOCKET_FD("receive_socket() : called on deleted CurlSocket, aborting", 0);
+      LT_LOG_DEBUG_SOCKET_FD_HANDLE("receive_socket() : called on deleted CurlSocket, aborting", 0);
       throw internal_error("CurlSocket::receive_socket(fd:" + std::to_string(fd) + ") called on deleted CurlSocket.");
     }
   }
@@ -54,13 +61,13 @@ CurlSocket::receive_socket(CURL* easy_handle, curl_socket_t fd, int what, CurlSt
 
     if (socket == nullptr) {
       // Assume libcurl calls this before closing the fd if it is in the idle connection pool.
-      LT_LOG_DEBUG_SOCKET_FD("receive_socket() : CURL_POLL_REMOVE with null socket", 0);
+      LT_LOG_DEBUG_SOCKET_FD_HANDLE("receive_socket() : CURL_POLL_REMOVE with null socket", 0);
       return 0;
     }
 
     // TODO: This should be throw internal_error, unless we re-add CURLOPT_CLOSESOCKETFUNCTION.
     if (!socket->is_open()) {
-      LT_LOG_DEBUG_SOCKET_FD("receive_socket() : socket already closed, deleting", 0);
+      LT_LOG_DEBUG_SOCKET_FD_HANDLE("receive_socket() : socket already closed, deleting", 0);
       delete socket;
       return 0;
     }
@@ -68,7 +75,7 @@ CurlSocket::receive_socket(CURL* easy_handle, curl_socket_t fd, int what, CurlSt
     if (socket->m_fileDesc != fd)
       throw internal_error("CurlSocket::receive_socket(fd:" + std::to_string(fd) + ") CURL_POLL_REMOVE fd mismatch.");
 
-    LT_LOG_DEBUG_SOCKET_FD("receive_socket() : CURL_POLL_REMOVE, removing from poll and deleting", 0);
+    LT_LOG_DEBUG_SOCKET_FD_HANDLE("receive_socket() : CURL_POLL_REMOVE, removing from poll and deleting", 0);
 
     this_thread::poll()->remove_and_close(socket);
 
@@ -96,27 +103,32 @@ CurlSocket::receive_socket(CURL* easy_handle, curl_socket_t fd, int what, CurlSt
     this_thread::poll()->open(socket);
     this_thread::poll()->insert_error(socket);
 
-    LT_LOG_DEBUG_SOCKET_FD("receive_socket() : new socket created and added to poll", 0);
+    LT_LOG_DEBUG_SOCKET_FD_HANDLE("receive_socket() : new socket created and added to poll", 0);
+
+    if (what == CURL_POLL_NONE) {
+      LT_LOG_DEBUG_SOCKET_FD_HANDLE("receive_socket() : CURL_POLL_NONE, not adding read or write", 0);
+      throw internal_error("CurlSocket::receive_socket(fd:" + std::to_string(fd) + ") called with CURL_POLL_NONE on new socket.");
+    }
   }
 
   if (!stack->is_running()) {
-    LT_LOG_DEBUG_SOCKET_FD("receive_socket() : CurlStack not running, aborting", 0);
+    LT_LOG_DEBUG_SOCKET_FD_HANDLE("receive_socket() : CurlStack not running, aborting", 0);
     throw internal_error("CurlSocket::receive_socket(fd:" + std::to_string(fd) + ") called while CurlStack is not running.");
   }
 
   if (what == CURL_POLL_NONE || what == CURL_POLL_OUT) {
-    LT_LOG_DEBUG_SOCKET_FD("receive_socket() : removing read", 0);
+    LT_LOG_DEBUG_SOCKET_FD_HANDLE("receive_socket() : removing read", 0);
     this_thread::poll()->remove_read(socket);
   } else {
-    LT_LOG_DEBUG_SOCKET_FD("receive_socket() : inserting read", 0);
+    LT_LOG_DEBUG_SOCKET_FD_HANDLE("receive_socket() : inserting read", 0);
     this_thread::poll()->insert_read(socket);
   }
 
   if (what == CURL_POLL_NONE || what == CURL_POLL_IN) {
-    LT_LOG_DEBUG_SOCKET_FD("receive_socket() : removing write", 0);
+    LT_LOG_DEBUG_SOCKET_FD_HANDLE("receive_socket() : removing write", 0);
     this_thread::poll()->remove_write(socket);
   } else {
-    LT_LOG_DEBUG_SOCKET_FD("receive_socket() : inserting write", 0);
+    LT_LOG_DEBUG_SOCKET_FD_HANDLE("receive_socket() : inserting write", 0);
     this_thread::poll()->insert_write(socket);
   }
 
