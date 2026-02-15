2a914bc249fe191f9ce7a83c21cba1c29c4604ca
rakshasa <sundell.software@gmail.com>
2026-02-15 15:52:59 +0100

    Stuff.

--- src/protocol/handshake_manager.cc
+++ src/protocol/handshake_manager.cc
@@ -166,12 +166,8 @@ HandshakeManager::create_outgoing(const sockaddr* sa, DownloadMain* download, in
   auto open_func = [&]() {
       int fd = open_and_connect_socket(connect_address.get());
 
-      if (fd == -1) {
-        // Need to call disconnect on peer list here as handshake->destroy_connection() doesn't get
-        // called.
-        download->peer_list()->disconnected(peer_info, 0);
+      if (fd == -1)
         return;
-      }
 
       int message;
 
@@ -195,12 +191,12 @@ HandshakeManager::create_outgoing(const sockaddr* sa, DownloadMain* download, in
   auto cleanup_func = [&]() {
       if (!handshake->is_open()) {
         LT_LOG_SA(sa, "failed to create outgoing connection: open failed", 0);
+        download->peer_list()->disconnected(peer_info, 0);
         return;
       }
 
       LT_LOG_SA(sa, "failed to create outgoing connection : socket manager triggered cleanup", 0);
 
-      // download->peer_list()->disconnected(peer_info, 0);
       handshake->destroy_connection(false);
     };
 
