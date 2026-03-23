3300a37bca871e37c2de6ce85d35b712083dec62
rakshasa <sundell.software@gmail.com>
2026-03-23 15:18:04 +0100

    Stuff.

--- src/torrent/data/block.cc
+++ src/torrent/data/block.cc
@@ -1,39 +1,3 @@
-// libTorrent - BitTorrent library
-// Copyright (C) 2005-2011, Jari Sundell
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-// In addition, as a special exception, the copyright holders give
-// permission to link the code of portions of this program with the
-// OpenSSL library under certain conditions as described in each
-// individual source file, and distribute linked combinations
-// including the two.
-//
-// You must obey the GNU General Public License in all respects for
-// all of the code used other than OpenSSL.  If you modify file(s)
-// with this exception, you may extend this exception to your version
-// of the file(s), but you are not obligated to do so.  If you do not
-// wish to do so, delete this exception statement from your version.
-// If you delete this exception statement from all source files in the
-// program, then also delete it here.
-//
-// Contact:  Jari Sundell <jaris@ifi.uio.no>
-//
-//           Skomakerveien 33
-//           3185 Skoppum, NORWAY
-
 #include "config.h"
 
 #include <algorithm>
@@ -106,7 +70,7 @@ Block::insert(PeerInfo* peerInfo) {
 
   return m_queued.emplace_back(block);
 }
-  
+
 void
 Block::erase(BlockTransfer* transfer) {
   if (transfer->is_erased())
@@ -251,7 +215,7 @@ Block::completed(BlockTransfer* transfer) {
   // We need to invalidate those unfinished and keep the one that
   // finished for later reference.
   remove_non_leader_transfers();
-  
+
   // We now know that all transfers except the current leader we're
   // handling has been invalidated.
   if (m_transfers.empty() || m_transfers.back() != transfer)
@@ -281,7 +245,7 @@ Block::transfer_dissimilar(BlockTransfer* transfer) {
   // Why not just delete? Gets done by completed(), though when
   // erasing the leader we need to remove dissimilar unless we have
   // another leader.
-  
+
   transfer->set_state(BlockTransfer::STATE_ERASED);
   transfer->set_position(0);
   transfer->set_block(NULL);
@@ -374,12 +338,13 @@ Block::invalidate_transfer(BlockTransfer* transfer) {
 
   if (transfer->peer_info() == NULL) {
     delete transfer;
+    assert(false && "Block::invalidate_transfer(...) transfer->peer_info() == NULL. This should not happen.");
     return; // Consider if this should be an exception.
   }
 
   m_notStalled -= (transfer->stall() == 0);
 
-  // Do the canceling magic here. 
+  // Do the canceling magic here.
   if (transfer->peer_info()->connection() != NULL)
     transfer->peer_info()->connection()->cancel_transfer(transfer);
 }
