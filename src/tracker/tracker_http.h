c0d17a1d06cc7f4f7c826d51881aa02b2656cfc8
Jari Sundell <sundell.software@gmail.com>
2026-05-02 08:34:38 +0200

    TrackerHttp now does requests for both IPv4 and IPv6.

--- src/tracker/tracker_http.h
+++ src/tracker/tracker_http.h
@@ -3,6 +3,7 @@
 
 #include <iosfwd>
 #include <memory>
+#include <tuple>
 
 #include "tracker/tracker_worker.h"
 #include "torrent/net/http_get.h"
@@ -24,18 +25,25 @@ public:
 
   bool                is_busy() const override;
 
+  void                close() override;
+
   void                send_event(tracker::TrackerState::event_enum new_state) override;
   void                send_scrape() override;
-
-  void                close() override;
 
 private:
   void                close_directly();
 
-  void                request_prefix(std::stringstream* stream, const std::string& url);
-  std::string         request_announce_url(tracker::TrackerState::event_enum state, TrackerParameters params, int family);
+  void                send_event_unsafe(tracker::TrackerState::event_enum state);
+  void                send_scrape_unsafe();
+  bool                send_next_family(bool scrape = false);
 
   void                delayed_send_scrape();
+
+  std::stringstream   request_prefix(const std::string& url);
+  std::string         request_announce_url(tracker::TrackerState::event_enum state, TrackerParameters params, int family);
+  std::tuple<int,int> request_families();
+
+  void                update_tracker_id(const std::string& id);
 
   void                receive_done();
   void                receive_signal_failed(const std::string& msg);
@@ -45,12 +53,16 @@ private:
   void                process_success(const Object& object);
   void                process_scrape(const Object& object);
 
-  void                update_tracker_id(const std::string& id);
-
   net::HttpGet                       m_get;
   std::shared_ptr<std::stringstream> m_data;
 
-  bool                  m_drop_deliminator{};
+  int                   m_hostname_family{};
+  int                   m_current_family{};
+  int                   m_next_family{};
+
+  bool                  m_last_success{};
+  std::string           m_last_error_message;
+
   std::string           m_current_tracker_id;
 
   bool                  m_requested_scrape{};
