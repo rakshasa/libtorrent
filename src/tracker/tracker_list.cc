321ca2b701e7ef09c45ea4a05fbaa4563961ba09
Jonathan Sanchez <sanchezthecoder@gmail.com>
2026-05-20 04:47:25 -0500

    Add tracker failure log group

--- src/tracker/tracker_list.cc
+++ src/tracker/tracker_list.cc
@@ -20,6 +20,9 @@
 #define LT_LOG(log_fmt, ...)                                            \
   lt_log_print_hash(LOG_TRACKER_EVENTS, info()->info_hash(), "tracker_list", log_fmt, __VA_ARGS__);
 
+#define LT_LOG_FAILURE(log_fmt, ...)                                    \
+  lt_log_print_hash(LOG_TRACKER_FAILURE_MESSAGES, info()->info_hash(), "tracker", log_fmt, __VA_ARGS__);
+
 namespace torrent {
 
 TrackerList::TrackerList() :
@@ -469,6 +472,7 @@ void
 TrackerList::receive_failed(tracker::Tracker tracker, const std::string& msg) {
   LT_LOG("received failure : requester:%p group:%u url:%s msg:'%s'",
          tracker.get_worker(), tracker.group(), tracker.url().c_str(), msg.c_str());
+  LT_LOG_FAILURE("Tracker: [%s]", msg.c_str());
 
   auto itr = find(tracker);
 
@@ -516,6 +520,7 @@ void
 TrackerList::receive_scrape_failed(tracker::Tracker tracker, const std::string& msg) {
   LT_LOG("received scrape failure : requester:%p group:%u url:%s msg:'%s'",
          tracker.get_worker(), tracker.group(), tracker.url().c_str(), msg.c_str());
+  LT_LOG_FAILURE("Tracker scrape: [%s]", msg.c_str());
 
   auto itr = find(tracker);
 
