820e72de96caf0f2451de146bb22c60da7a87aa4
Jari Sundell <sundell.software@gmail.com>
2026-06-10 16:46:30 +0200

    Cleaned up ResourceManager and fixed potential empty container issue.

--- src/torrent/download/resource_manager.h
+++ src/torrent/download/resource_manager.h
@@ -1,39 +1,3 @@
-// libTorrent - BitTorrent library
-// Copyright (C) 2005-2011, Jari Sundell
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-// In addition, as a special exception, the copyright holders give
-// permission to link the code of portions of this program with the
-// OpenSSL library under certain conditions as described in each
-// individual source file, and distribute linked combinations
-// including the two.
-//
-// You must obey the GNU General Public License in all respects for
-// all of the code used other than OpenSSL.  If you modify file(s)
-// with this exception, you may extend this exception to your version
-// of the file(s), but you are not obligated to do so.  If you do not
-// wish to do so, delete this exception statement from your version.
-// If you delete this exception statement from all source files in the
-// program, then also delete it here.
-//
-// Contact:  Jari Sundell <jaris@ifi.uio.no>
-//
-//           Skomakerveien 33
-//           3185 Skoppum, NORWAY
-
 #ifndef LIBTORRENT_PEER_RESOURCE_MANAGER_H
 #define LIBTORRENT_PEER_RESOURCE_MANAGER_H
 
@@ -86,16 +50,13 @@ private:
 };
 
 
-class LIBTORRENT_EXPORT ResourceManager :
-    private std::vector<resource_manager_entry>,
-    private std::vector<choke_group*> {
+class LIBTORRENT_EXPORT ResourceManager : private std::vector<resource_manager_entry> {
 public:
-  using base_type       = std::vector<resource_manager_entry>;
-  using choke_base_type = std::vector<choke_group*>;
-  using value_type      = base_type::value_type;
-  using iterator        = base_type::iterator;
+  using base_type        = std::vector<resource_manager_entry>;
+  using value_type       = base_type::value_type;
+  using iterator         = base_type::iterator;
 
-  using group_iterator = choke_base_type::iterator;
+  using choke_group_list = std::vector<std::unique_ptr<choke_group>>;
 
   using base_type::begin;
   using base_type::end;
@@ -114,29 +75,28 @@ public:
   iterator            find_throw(const DownloadMain* d);
   iterator            find_group_end(uint16_t group);
 
-  unsigned int            group_size() const                    { return choke_base_type::size(); }
-  choke_group*            group_back()                          { return choke_base_type::back(); }
+  unsigned int        group_size() const                         { return m_choke_groups.size(); }
+  choke_group*        group_back()                               { return m_choke_groups.back().get(); }
 
-  choke_group*            group_at(uint16_t grp);
-  choke_group*            group_at_name(const std::string& name);
+  choke_group*        group_at(uint16_t grp);
+  choke_group*        group_at_name(const std::string& name);
 
-  int                     group_index_of(const std::string& name);
+  int                 group_index_of(const std::string& name);
 
-  group_iterator          group_begin() { return choke_base_type::begin(); }
-  group_iterator          group_end()   { return choke_base_type::end(); }
+  auto                group_begin()                              { return m_choke_groups.begin(); }
+  auto                group_end()                                { return m_choke_groups.end(); }
 
-  resource_manager_entry& entry_at(const DownloadMain* d) { return *find_throw(d); }
+  auto&               entry_at(const DownloadMain* d)            { return *find_throw(d); }
 
   static void         set_priority(iterator itr, uint16_t pri);
   void                set_group(iterator itr, uint16_t grp);
 
-  // When setting this, make sure you choke peers, else change
-  // receive_can_unchoke.
-  unsigned int        currently_upload_unchoked() const         { return m_currentlyUploadUnchoked; }
-  unsigned int        currently_download_unchoked() const       { return m_currentlyDownloadUnchoked; }
+  // When setting this, make sure you choke peers, else change receive_can_unchoke.
+  unsigned int        currently_upload_unchoked() const          { return m_currentlyUploadUnchoked; }
+  unsigned int        currently_download_unchoked() const        { return m_currentlyDownloadUnchoked; }
 
-  unsigned int        max_upload_unchoked() const               { return m_maxUploadUnchoked; }
-  unsigned int        max_download_unchoked() const             { return m_maxDownloadUnchoked; }
+  unsigned int        max_upload_unchoked() const                { return m_maxUploadUnchoked; }
+  unsigned int        max_download_unchoked() const              { return m_maxDownloadUnchoked; }
 
   void                set_max_upload_unchoked(unsigned int m);
   void                set_max_download_unchoked(unsigned int m);
@@ -162,11 +122,13 @@ private:
 
   int                 balance_unchoked(unsigned int weight, unsigned int max_unchoked, bool is_up);
 
-  unsigned int        m_currentlyUploadUnchoked{0};
-  unsigned int        m_currentlyDownloadUnchoked{0};
+  unsigned int        m_currentlyUploadUnchoked{};
+  unsigned int        m_currentlyDownloadUnchoked{};
 
-  unsigned int        m_maxUploadUnchoked{0};
-  unsigned int        m_maxDownloadUnchoked{0};
+  unsigned int        m_maxUploadUnchoked{};
+  unsigned int        m_maxDownloadUnchoked{};
+
+  choke_group_list    m_choke_groups;
 };
 
 } // namespace torrent
