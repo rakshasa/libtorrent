ad128901ad00d5a685d8b726a42bd3295a0b2aa8
Jari Sundell <sundell.software@gmail.com>
2026-06-21 16:05:36 +0200

    Added backoff to callback when it is the only events being handled.

--- src/torrent/system/poll_kqueue.cc
+++ src/torrent/system/poll_kqueue.cc
@@ -74,6 +74,8 @@ public:
   unsigned int        m_waiting_events{};
   unsigned int        m_changed_events{};
 
+  unsigned int        m_callback_interrupt_backoff{};
+
   Table                            m_table;
   std::unique_ptr<struct kevent[]> m_events;
   std::unique_ptr<struct kevent[]> m_changes;
@@ -210,15 +212,25 @@ Poll::do_poll(std::chrono::microseconds timeout) {
 
 int
 Poll::poll(std::chrono::microseconds timeout) {
+  auto previous_state = m_polling_state.fetch_or(flag_polling, std::memory_order_acquire);
+
+  bool callback_interrupting{};
+
+  if (previous_state & flag_interrupted || system::Thread::self()->has_any_callbacks()) {
+    auto backoff_shift = std::min(m_internal->m_callback_interrupt_backoff, 3u);
+
+    if (backoff_shift > 0)
+      timeout = std::min(1000us * (1 << (backoff_shift - 1)), timeout);
+    else
+      timeout = 0us;
+
+    callback_interrupting = true;
+  }
+
   timespec timeout_spec = {
     static_cast<time_t>(timeout.count() / 1000000),
     static_cast<long>(timeout.count() % 1000000) * 1000
   };
-
-  auto previous_state = m_polling_state.fetch_or(flag_polling, std::memory_order_acquire);
-
-  if (previous_state & flag_interrupted || system::Thread::self()->has_any_callbacks())
-    timeout_spec = timespec{0, 0};
 
   int nfds = ::kevent(m_internal->m_fd,
                       m_internal->m_changes.get(),
@@ -239,6 +251,11 @@ Poll::poll(std::chrono::microseconds timeout) {
 
   if (nfds == -1)
     return -1;
+
+  if (nfds == 1 && callback_interrupting && m_internal->m_events[0].filter == EVFILT_USER)
+    m_internal->m_callback_interrupt_backoff++;
+  else
+    m_internal->m_callback_interrupt_backoff = 0;
 
   m_internal->m_waiting_events = nfds;
   return nfds;
