2eff23e31f5fbe298f9269b8acf15377f204fe22
Jari Sundell <sundell.software@gmail.com>
2026-07-09 19:02:54 +0200

    Moved all sync/diskspace related methods to MemoryManager.

--- src/data/chunk_list.cc
+++ src/data/chunk_list.cc
@@ -65,6 +65,8 @@ ChunkList::clear() {
     clear_chunk(chunk, release_default);
   }
 
+  runtime::memory_manager()->release_sync_queue(m_queue.size(), m_chunk_size);
+
   m_queue.clear();
 
   if (std::any_of(begin(), end(), std::mem_fn(&ChunkListNode::chunk)))
@@ -195,6 +197,8 @@ ChunkList::release(ChunkHandle* handle, release_flags flags) {
       // this branch.
       m_queue.push_back(handle->object());
 
+      runtime::memory_manager()->account_sync_queue(m_chunk_size);
+
     } else {
       handle->object()->dec_rw();
     }
@@ -273,7 +277,9 @@ ChunkList::sync_chunks(cache_list& cache, sync_flags flags) {
   // If we got enough diskspace and have not requested safe syncing,
   // then sync all chunks with MS_ASYNC.
   if (!(flags & (sync_safe | sync_sloppy))) {
-    if (m_manager->safe_sync() || m_slot_free_diskspace(cache) <= m_manager->safe_free_diskspace())
+    if (runtime::memory_manager()->safe_sync())
+      flags = flags | sync_safe;
+    else if (m_slot_free_diskspace(cache) <= runtime::memory_manager()->sync_safe_free_diskspace())
       flags = flags | sync_safe;
     else
       flags = flags | sync_force;
@@ -315,6 +321,8 @@ ChunkList::sync_chunks(cache_list& cache, sync_flags flags) {
     instrumentation_update(INSTRUMENTATION_MINCORE_SYNC_NOT_SYNCED, std::distance(m_queue.begin(), split));
     instrumentation_update(INSTRUMENTATION_MINCORE_SYNC_NOT_DEALLOCATED, std::count_if(split, m_queue.end(), std::mem_fn(&ChunkListNode::is_valid)));
   }
+
+  runtime::memory_manager()->release_sync_queue(std::distance(split, m_queue.end()), m_chunk_size);
 
   m_queue.erase(split, m_queue.end());
 
@@ -372,7 +380,7 @@ inline bool
 ChunkList::check_node(ChunkListNode* node) {
   return
     node->time_modified() != 0us &&
-    node->time_modified() + std::chrono::seconds(m_manager->timeout_sync()) < this_thread::cached_time();
+    node->time_modified() + runtime::memory_manager()->timeout_sync() < this_thread::cached_time();
 }
 
 // Optimize the selection of chunks to sync. Continuous regions are
--- src/torrent/runtime/memory_manager.h
+++ src/torrent/runtime/memory_manager.h
@@ -12,6 +12,10 @@ public:
 
   // Should we allow the client to reserve some memory?
 
+  //
+  // Memory Management:
+  //
+
   uint64_t            memory_usage() const;
   uint32_t            memory_block_count() const;
 
@@ -21,21 +25,92 @@ public:
   void                account_memory_block(uint64_t bytes);
   void                release_memory_block(uint64_t bytes);
 
+  //
+  // Disk Sync and Preload Management:
+  //
+
+  uint32_t            sync_queue_block_count() const;
+  uint64_t            sync_queue_memory_usage() const;
+
+  uint64_t            sync_safe_free_diskspace() const;
+
+  bool                safe_sync() const;
+  void                set_safe_sync(bool state);
+
+  // Set the wait time after last write to a chunk before attempting to sync.
+  //
+  // Longer timeout allows the kernel to sync at its convenience.
+  auto                timeout_sync() const;
+  void                set_timeout_sync(uint32_t seconds);
+
+  // Set to 0 to disable preloading.
+  //
+  // How the value is used is yet to be determined, but it won't be able to use actual requests in
+  // the request queue as we can easily stay ahead of it causing preloading to fail.
+  uint32_t            preload_type() const;
+  void                set_preload_type(uint32_t preload_type);
+
+  uint32_t            preload_min_size() const;
+  void                set_preload_min_size(uint32_t bytes);
+
+  // Required rate (per integer MB of remaining chunk) before attempting to preload chunk.
+  uint32_t            preload_required_rate() const;
+  void                set_preload_required_rate(uint32_t bytes);
+
+  uint32_t            stats_preloaded() const;
+  uint32_t            stats_not_preloaded() const;
+
 protected:
+  friend class torrent::ChunkList;
+  friend class torrent::PeerConnectionBase;
   friend class torrent::RuntimeManager;
 
   MemoryManager();
   ~MemoryManager();
 
+  void                account_sync_queue(uint64_t bytes);
+  void                release_sync_queue(uint32_t count, uint32_t chunk_size);
+
+  void                increment_stats_preloaded();
+  void                increment_stats_not_preloaded();
+
 private:
   std::atomic<uint64_t> m_memory_usage{};
   std::atomic<uint32_t> m_memory_block_count{};
   std::atomic<uint64_t> m_max_memory_usage{};
+
+  std::atomic<uint32_t> m_sync_queue_block_count{};
+  std::atomic<uint64_t> m_sync_queue_memory_usage{};
+
+  std::atomic<bool>     m_safe_sync{false};
+  std::atomic<uint32_t> m_timeout_sync{600};
+
+  std::atomic<uint32_t> m_preload_type{0};
+  std::atomic<uint32_t> m_preload_min_size{256 << 10};
+  std::atomic<uint32_t> m_preload_required_rate{5 << 10};
+
+  std::atomic<uint32_t> m_stats_preloaded{};
+  std::atomic<uint32_t> m_stats_not_preloaded{};
 };
 
-inline uint64_t MemoryManager::memory_usage() const       { return m_memory_usage.load(std::memory_order_acquire); }
-inline uint32_t MemoryManager::memory_block_count() const { return m_memory_block_count.load(std::memory_order_acquire); }
-inline uint64_t MemoryManager::max_memory_usage() const   { return m_max_memory_usage.load(std::memory_order_acquire); }
+inline uint64_t MemoryManager::memory_usage() const            { return m_memory_usage.load(std::memory_order_acquire); }
+inline uint32_t MemoryManager::memory_block_count() const      { return m_memory_block_count.load(std::memory_order_acquire); }
+inline uint64_t MemoryManager::max_memory_usage() const        { return m_max_memory_usage.load(std::memory_order_acquire); }
+inline uint32_t MemoryManager::sync_queue_block_count() const  { return m_sync_queue_block_count.load(std::memory_order_acquire); }
+inline uint64_t MemoryManager::sync_queue_memory_usage() const { return m_sync_queue_memory_usage.load(std::memory_order_acquire); }
+
+inline bool     MemoryManager::safe_sync() const               { return m_safe_sync.load(std::memory_order_acquire); }
+inline void     MemoryManager::set_safe_sync(bool state)       { m_safe_sync.store(state, std::memory_order_release); }
+inline auto     MemoryManager::timeout_sync() const            { return std::chrono::seconds(m_timeout_sync.load(std::memory_order_acquire)); }
+inline uint32_t MemoryManager::preload_type() const            { return m_preload_type.load(std::memory_order_acquire); }
+inline uint32_t MemoryManager::preload_min_size() const        { return m_preload_min_size.load(std::memory_order_acquire); }
+inline uint32_t MemoryManager::preload_required_rate() const   { return m_preload_required_rate.load(std::memory_order_acquire); }
+
+inline uint32_t MemoryManager::stats_preloaded() const         { return m_stats_preloaded.load(std::memory_order_acquire); }
+inline uint32_t MemoryManager::stats_not_preloaded() const     { return m_stats_not_preloaded.load(std::memory_order_acquire); }
+
+inline void     MemoryManager::increment_stats_preloaded()     { m_stats_preloaded.fetch_add(1, std::memory_order_acq_rel); }
+inline void     MemoryManager::increment_stats_not_preloaded() { m_stats_not_preloaded.fetch_add(1, std::memory_order_acq_rel); }
 
 } // namespace torrent::runtime
 
