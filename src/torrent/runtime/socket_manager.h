b9ddadec4a1ab9f6a40271f2a0be88e0cb025e49
xirvik <community@xirvik.net>
2026-08-08 15:15:01 +0000

    Refuse a files allocation outside what the file manager accepts.
    FileManager takes 4 to 2^16; outside that it threw from a callback and exited.

--- src/torrent/runtime/socket_manager.h
+++ src/torrent/runtime/socket_manager.h
@@ -54,6 +54,8 @@ public:
   static constexpr uint32_t category_count     = 5;
   static constexpr uint32_t category_max_alloc = 1000000;
   static constexpr uint32_t http_max_alloc     = 4096;
+  static constexpr uint32_t files_max_alloc    = 1 << 16;
+  static constexpr uint32_t files_min_alloc    = 4;
   static constexpr int      flag_inactive = (1 << 0);
 
   using category_t = socket_manager_category_t;
@@ -68,6 +70,7 @@ public:
   uint32_t            category_max_allocation(category_t category);
 
   uint32_t            category_alloc_limit(category_t category);
+  uint32_t            category_alloc_minimum(category_t category);
 
   uint32_t            generic_min_allocation();
   uint32_t            reserved_allocation();
