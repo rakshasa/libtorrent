59134467dff9208dc8ec48f5b301386edcdde8c8
xirvik <community@xirvik.net>
2026-08-20 04:57:59 +0000

    Reject a bencode string length that does not fit in unsigned int.
    The accumulator wrapped, so 4294967300: was accepted as a four byte string.

--- test/torrent/object_stream_test.h
+++ test/torrent/object_stream_test.h
@@ -9,6 +9,7 @@ class ObjectStreamTest : public CppUnit::TestFixture {
   CPPUNIT_TEST(testOutputMask);
   CPPUNIT_TEST(testBuffer);
   CPPUNIT_TEST(testReadBencodeC);
+  CPPUNIT_TEST(test_read_string_length);
 
   CPPUNIT_TEST(test_read_skip);
   CPPUNIT_TEST(test_read_skip_invalid);
@@ -22,6 +23,7 @@ public:
   void testBuffer();
 
   void testReadBencodeC();
+  void test_read_string_length();
 
   void test_read_skip();
   void test_read_skip_invalid();
