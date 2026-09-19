26b17d9b6ac1c873d374b9caebd5b737dc4fcd23
xirvik <community@xirvik.net>
2026-09-19 02:11:53 +0000

    Fix the signal mask, the reported cause and the lost handler on a chunk write bus error
    Recover only when the thread armed it, and leave other bus errors to the old handler.

--- test/data/test_chunk.h
+++ test/data/test_chunk.h
@@ -4,9 +4,11 @@ class test_chunk : public test_fixture {
   CPPUNIT_TEST_SUITE(test_chunk);
 
   CPPUNIT_TEST(test_buffer_rejects_wrapping_range);
+  CPPUNIT_TEST(test_from_buffer_bus_error_keeps_signal_mask);
 
   CPPUNIT_TEST_SUITE_END();
 
 public:
   void test_buffer_rejects_wrapping_range();
+  void test_from_buffer_bus_error_keeps_signal_mask();
 };
